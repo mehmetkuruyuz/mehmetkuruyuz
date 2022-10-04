685d53e1d47ce9da401f9d1483050e17ff2478aa
Mehmet KURUYÜZ <mehmetkuruyuz@gmail.com>
2022-10-04 11:56:30 +0300

    updates of changelog
    updates of changelog

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,283 @@
+<!doctype html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>mehmet - php developer</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/railscasts.min.css">
+      <link rel="stylesheet" href="/scribbler-global.css">
+      <link rel="stylesheet" href="/scribbler-landing.css">
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0ENKSSJ1D0"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-0ENKSSJ1D0');
+    </script>
+  </head>
+  <body>
+    <nav>
+      <div class="logo"></div>
+      <ul class="menu">
+        <div class="menu__item toggle"><span></span></div>
+        <li class="menu__item"><a href="/" class="link link--dark"><i class="fa fa-book"></i> since 2006</a></li>
+      </ul>
+    </nav>
+    <div class="hero">
+      <h1 class="hero__title">merhabalar</h1>
+      <p class="hero__description">hakkımda</p>
+    </div>
+    <div class="hero__terminal">
+      <pre>
+        <!-- Place your demo code here -->
+        <code class="shell-session demo"> ~ $ php artisan clear:cache </code>
+      </pre>
+    </div>
+    <div class="wrapper">
+      <div class="installation">
+        <h3 class="section__title">iletişim</h3>
+        <div class="tab__container">
+          <ul class="tab__menu">
+            <li class="tab active" data-tab="email">mail</li>
+
+            <li class="tab" data-tab="linkedin">linkedin</li>
+            <li class="tab" data-tab="github">github</li>
+          </ul>
+          <pre class="nohighlight code">
+            <code class="tab__pane active email"> info [at] mekryz.com </code>
+            <code class="tab__pane linkedin"><a href="https://www.linkedin.com/in/mehmet-kuruyuz" target="_blank"><i class="fa fa-linkedin"></i> Linked in</a> </code>
+            <code class="tab__pane github"><a href="https://github.com/mehmetkuruyuz/" target="_blank"><i class="fa fa-github"></i> Github</a></code>
+          </pre>
+        </div>
+      </div>
+      <hr />
+      <h3 class="section__title keybinding__detail">What i use</h3>
+
+      <div class="feature">
+        <div class="feature__item">
+          <h3 class="section__title">php <i class="fa fa-battery-full" aria-hidden="true"></i></h3>
+          <p class="p_tr">Php 4.3'den bu yana kullanmış olduğum ana script dilidir. Version değişiklikleri ve sonrasında özellikle oop ve diğer güncelleştirmeler ile birlikte 2006'dan aktif iş yaşantımın %80'ini oluşturmaktadır.</p>
+        </div>
+
+        <div class="feature__item">
+          <h3 class="section__title">laravel <i class="fa fa-battery-full" aria-hidden="true"></i></h3>
+          <p>Laravel 5.2'den sonra komple üzerinde devam ettiğim aktif olarak 5-6-7-8 ve şuan 9 sürümü ile devam ettiğim çalışma ortamı.</p>
+        </div>
+
+        <div class="feature__item">
+          <h3 class="section__title">zend framework <i class="fa fa-battery-half" aria-hidden="true"></i></h3>
+          <p class="p_tr">2008 yılında multi ve dinamik subdomain altyapısını kullanan bir proje için kullanmış olduğum php framework'ü.</p>
+        </div>
+
+        <div class="feature__item">
+          <h3 class="section__title">mysql <i class="fa fa-battery-full" aria-hidden="true"></i></h3>
+          <p>2006 yılından itibaren genel olarak tüm projelerimde kullandığım relational veritabanı. </p>
+        </div>
+        <div class="feature__item">
+          <h3 class="section__title">elastic search <i class="fa fa-battery-half" aria-hidden="true"></i></h3>
+          <p>Ara, bul ve hedefle, full text arama için kullanmış olduğum 2022 yılı üzerinde uzun süreler harcadığım no-sql veritabanı. </p>
+        </div>
+
+        <div class="feature__item">
+          <h3 class="section__title">pure javascript <i class="fa fa-battery-full" aria-hidden="true"></i></h3>
+        </div>
+        <div class="feature__item">
+          <h3 class="section__title">jquery framework <i class="fa fa-battery-full" aria-hidden="true"></i></h3>
+        </div>
+        <div class="feature__item">
+          <h3 class="section__title">vue framework<i class="fa fa-battery-half" aria-hidden="true"></i></h3>
+        </div>
+        <div class="feature__item">
+          <h3 class="section__title">css  <i class="fa fa-battery-three-quarters" aria-hidden="true"></i></h3>
+        </div>
+        <div class="feature__item">
+          <h3 class="section__title">bootstrap  <i class="fa fa-battery-full" aria-hidden="true"></i></h3>
+        </div>
+        <div class="feature__item">
+          <h3 class="section__title">ui kit  <i class="fa fa-battery-full" aria-hidden="true"></i></h3>
+        </div>
+        <div class="feature__item">
+          <h3 class="section__title">delphi (object pascal)  <i class="fa fa-battery-half" aria-hidden="true"></i></h3>
+        </div>
+      </div>
+
+      <div class="keybinding">
+        <ul class="keybinding__detail">
+          <h3 class="keybinding__title">2023 hedefleri</h3>
+          <li>flutter <span class="keybinding__label">mobil</span></li>
+          <li>python <span class="keybinding__label">data analiz</span></li>
+          <li>react <span class="keybinding__label">frontend</span></li>
+          <li>mongodb <span class="keybinding__label">database</span></li>
+        </ul>
+        <ul class="keybinding__detail">
+          <h3 class="keybinding__title">last github</h3>
+          <li><span class="keybinding__label">private repo</span> test</li>
+          <li><span class="keybinding__label">private repo</span> test</li>
+          <li><span class="keybinding__label">private repo</span> test</li>
+          <li><span class="keybinding__label">private repo</span> test</li>
+        </ul>
+      </div>
+      <div class="callout">
+        <p></p>
+      </div>
+    </div>
+    <div class="changelog">
+      <div class="wrapper">
+        <h3 class="section__title">changelog</h3>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v2022.12 senior backend developer<br /><small>optimum 7 | remote</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>eticaret sistemleri, bigcommerce entegre sistemleri, shopify entegre sistemleri</li>
+              <li>kullanılan teknolojiler: php , mysql, javascript, css, redis, elastic search</li>
+              <li>kullanılan frameworkler: jquery, laravel 5-8,bootstrap </li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v2021.11 - senior full stack developer <br /><small>nar10 | remote</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>eticaret sistemleri, erp sistemleri,point sale yazılımları,uygulama  güncelleştirmeleri</li>
+              <li>kullanılan teknolojiler: php , mysql, javascript, css,redis</li>
+              <li>kullanılan frameworkler: jquery, laravel 5-8,bootstrap </li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v2021.10  senior backend developer<br /><small>Postha UA | remote</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>ödeme yöntemleri entegresi, kargo uygulamaları, özel harita entegrasyonları,cms sistemi</li>
+              <li>kullanılan teknolojiler: php , mysql, javascript, css,redis</li>
+              <li>kullanılan frameworkler: jquery, laravel 5-8,bootstrap </li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v2020.09  senior backend developer<br /><small>crea bilişim | remote</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>eticaret sistemleri, erp sistemleri, ödeme yöntemleri entegrasyonu, gümrük yazılımları</li>
+              <li>kullanılan teknolojiler: php , mysql, javascript, css</li>
+              <li>kullanılan frameworkler: jquery, laravel 5-8, vue </li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v2019.08  senior full stack developer<br /><small>bsy information and tech a.s. | remote</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>cms sistemleri, özel üretim erp sistemleri,sağlık uygulamaları, data yönetimi,informasyon sistemleri</li>
+              <li>kullanılan teknolojiler: php , mysql, javascript, css, delphi</li>
+              <li>kullanılan frameworkler: jquery, laravel 5-7,bootstrap,vue </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v07 - senior full stack developer <br /><small>türkiye klinikleri | ankara</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>cms sistemleri, media streaming sistemleri, video analiz ve istatistik alanları</li>
+              <li>kullanılan teknolojiler: php , mysql, javascript, css</li>
+              <li>kullanılan frameworkler: jquery, laravel 5.5,bootstrap </li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v06 - technology lead <br /><small> moonlife  | ankara</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>erp yazılımı, üretim yazılımı, destek sistemleri üretimi, firma içi teknoloji yatırımları kontrolü</li>
+              <li>kullanılan teknolojiler: php , mysql, javascript, css</li>
+              <li>kullanılan frameworkler: jquery, laravel 5.2, smarty 3, bootstrap,vue</li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v05 - senior full stack developer <br /><small> modalife mobilya | ankara</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>eticaret sisteminin hayata geçirilmesi. </li>
+              <li>kullanılan teknolojiler: php , mysql, javascript, css</li>
+              <li>kullanılan frameworkler: jquery, zend, smarty</li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v04 - mid-level full stack developer <br /><small> yön group  | ankara</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>Firma bünyesinde dinamik subdomain, chat ve multi dil seçenekli pazar yerinin geliştirilmesi. bakımı ve optimizasyonları </li>
+              <li>firmaya ait cms sisteminin geliştirilmesi, bakımı müşteri panellerinin kurulumu ve seo işlemleri.</li>
+              <li>kullanılan teknolojiler: php , mysql, javascript, css</li>
+              <li>kullanılan frameworkler: jquery, zend, smarty</li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v03 - junior full stack developer <br /><small> pleksus  | ankara</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>Pleksus Bilişim bünyesinde bulunan konferans sistemlerinin bakımı, geliştirilmesi, ölçeklendirilmesi. </li>
+              <li>Firma bünyesinde özel projelerin geliştirilmesi. </li>
+              <li>kullanılan teknolojiler: php , mysql, javascript, css</li>
+              <li>kullanılan frameworkler: jquery</li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v02 - web developer <br /><small> çağ ajans | ankara</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>Çağ ajans bünyesinde bulunan müşteriler ait web sitelerinin düzenlenmesi, bakımları</li>
+              <li>firmaya ait php cms sistemlerinin kurulumu, bakımı ve geliştirilmesi</li>
+              <li>kullanılan teknolojiler: php , mysql, javascript, css</li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v01 - web developer <br /><small> rituel ajans | ankara</small></h4>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>Rituel Ajans  müşterilerinin web projelerini ayağa kaldırmak</li>
+              <li>Firmaya bünyesinde ki tüm web sitelerinin bakımını sağlamak</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
+    <script src="/scribbler.js"></script>
+  </body>
+</html>
